dfd54b91ad90215f3ef439ce79cd85bb4097eccd
suraj-udupa <surajudupa@outlook.com>
2017-06-25 20:20:34 +0530

    Added negative case

--- ThinkSimpleStruct/src/test/java/com/thinksimple/test/TestController.java
+++ ThinkSimpleStruct/src/test/java/com/thinksimple/test/TestController.java
@@ -1,19 +1,29 @@
 package com.thinksimple.test;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import com.thinksimple.controller.RestController;
 
 import junit.framework.TestCase;
 
-public class TestController extends TestCase {
+public class TestController {
 
 	
 	@Test
-	public void testAdd1Plus1() 
+	public void PositiveCase() 
 	{
 	    RestController control = new RestController();
 	    
 	    assertEquals("Server Response", control.restResponse());
+	}
+	
+	@Test
+	public void NegativeCase() 
+	{
+	    RestController control = new RestController();
+	    
+	    assertEquals("ServerResponse", control.restResponse());
 	}
 }
